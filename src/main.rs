e2e12668af34c918c1ee37585cbad81888ed3790
mahanfarzaneh2000 <mahan.farzaneh2@gmail.com>
2021-11-03 22:55:27 +0330

    changed lexing method

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,20 @@ use std::io::{ BufRead, BufReader};
 use std::env;
 use std::fs::File;
 
+// enum TokenType {
+// 	STRING,PRINT
+// }
+
 #[derive(Debug)]
-enum TokenTypes {
-	HALT,NOP
+#[derive(PartialEq)]
+enum LiteralToken{
+	STRING,CHAR,NONE
 }
 
-struct Token {
-	token_type:TokenTypes,
-	literal:String,
-	line:u32,
-	col:u32
-}
+// struct Token{
+// 	token_type: TokenType,
+// 	value: String,
+// }
 
 fn main() {
   let args:Vec<_> = env::args().collect();
@@ -31,66 +34,68 @@ fn main() {
   }
 }
 
-fn scan_code_file(file_path:&str) -> u8 {
+fn scan_code_file(file_path:&str) {
 	let mut f = BufReader::new(File::open(file_path).expect("ERROR: File not Found"));
-	let mut token_stack = Vec::<Token>::new();
 
 	let mut buf = Vec::<u8>::new();
 	let mut col_number : u32 = 1;
 	let mut line_number : u32 = 1;
 	while f.read_until(b'\n', &mut buf).expect("read_until failed") != 0 {
 		let s = String::from_utf8(buf).expect("from_utf8 failed");
-		let mut word_buffer = String::new();
+
+		let mut buffer = String::new();
+		let mut literal_token_buffer = LiteralToken::NONE;
+
 		for c in s.chars() {
-			// end of the line
-			if col_number == s.len() as u32{
-				word_buffer.push(c);
-				if word_buffer.len() > 0 {
-					//println!("word: {},line: {},col: {}",word_buffer.trim_end(),line_number,col_number-(word_buffer.len() as u32)+1);
-					let token = create_token(word_buffer.trim_end(),line_number,col_number-(word_buffer.len() as u32)+1);
-					token_stack.push(token);
+			if c == '\n'{
+				if literal_token_buffer != LiteralToken::NONE {
+					panic!("ERROR: Unterminated literal token");
 				}
-				word_buffer.clear();
-			}
-			else if  c == ' ' || c == '\t' || c=='\n'{
-				if word_buffer.len() > 0 {
-					//println!("word: {},line: {},col: {}",word_buffer,line_number,col_number-(word_buffer.len() as u32));
-					let token = create_token(word_buffer.trim_end(),line_number,col_number-(word_buffer.len() as u32));
-					token_stack.push(token);
+				if buffer.len() > 0 {
+					println!("#{}#" , buffer);
 				}
-				word_buffer.clear();
-			}
-			else{
-				word_buffer.push(c);
+				buffer.clear();
+			} else if  c == ' ' || c == '\t' || c == '\n' || c == '\r' {
+				if literal_token_buffer != LiteralToken::NONE {
+					buffer.push(c);
+				}else{
+					if buffer.len() > 0 {
+						println!("#{}#", buffer);
+					}
+					buffer.clear();
+				}
+			} else if c == '"'  {
+				if literal_token_buffer == LiteralToken::NONE {
+					literal_token_buffer = LiteralToken::STRING;
+				}else if literal_token_buffer == LiteralToken::STRING {
+					literal_token_buffer = LiteralToken::NONE;
+					println!("\"{}\"",buffer);
+					buffer.clear();
+				}else{
+					buffer.push(c);
+				}
+			} else if c == '\''{
+				if literal_token_buffer == LiteralToken::NONE {
+					literal_token_buffer = LiteralToken::CHAR;
+				}else if literal_token_buffer == LiteralToken::CHAR {
+					literal_token_buffer = LiteralToken::NONE;
+					println!("'{}'",buffer);
+					buffer.clear();
+				}else{
+					buffer.push(c);
+				}
+			}else{
+				buffer.push(c);
 			}
 			col_number +=1;
 		}
+		println!("#{}#",buffer);
+		buffer.clear();
 		line_number += 1;
 		col_number = 1;
 		// this returns the ownership of the read data to buf
 		buf = s.into_bytes();
 		buf.clear();
 	}
-	for token in token_stack{
-		println!("type:{:?}, literal:{}, line:{}, col:{}",token.token_type,token.literal,token.line,token.col);
-	}
-	0
-}
-
-fn create_token(word: &str, line: u32, col: u32) -> Token {
-	if word == "halt" {
-		Token {
-			token_type:TokenTypes::HALT,
-			literal:word.to_string(),
-			line,
-			col,
-		}
-	}else {
-		Token {
-			token_type:TokenTypes::NOP,
-			literal:word.to_string(),
-			line,
-			col
-		}
-	}
+	println!("{}:{}", line_number, col_number);
 }
